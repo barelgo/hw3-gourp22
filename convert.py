19611c3b375e856c0e433d5994ffac6cd01066b4
barelgo <44042728+barelgo@users.noreply.github.com>
2018-11-22 18:56:25 +0100

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,18 @@
+#import the libraries needed
+
+import pandas as pd
+
+#read the csv file
+df = pd.read_csv('Airbnb_Texas_Rentals.csv',index_col=0)
+
+df['description'].fillna(' ',inplace=True)
+
+df['title'].fillna(' ',inplace=True)
+
+#create .tsv files for every doc.
+
+for i in range(len(df)):
+
+    name = 'directory/doc_{}.tsv'.format(i+1)
+
+    df.iloc[i].to_csv(name,sep='\t', index=False)
